168d4502cf402ca80b8808d37efae174e77d25d2
Yvan FEREZ <Linzdigr@users.noreply.github.com>
2025-08-06 16:00:56 +0200

    initial commit

---
+++ testing_display_functions.c
@@ -0,0 +1,190 @@
+#include <zephyr.h>
+#include <drivers/display.h>
+
+enum corner {
+	TOP_LEFT,
+	TOP_RIGHT,
+	BOTTOM_RIGHT,
+	BOTTOM_LEFT
+};
+
+typedef void(*fill_buffer)(enum corner corner, uint8_t grey, uint8_t *buf,
+              size_t buf_size);
+
+static uint16_t get_rgb565_color(enum corner corner, uint8_t grey) {
+  uint16_t color = 0;
+  uint16_t grey_5bit;
+
+  switch (corner)
+  {
+  case TOP_LEFT:
+    color = 0xF800u;
+    break;
+  case TOP_RIGHT:
+    color = 0x07E0u;
+    break;
+  case BOTTOM_RIGHT:
+    color = 0x001Fu;
+    break;
+  case BOTTOM_LEFT:
+    grey_5bit = grey & 0x1Fu;
+    /* shift the green an extra bit, it has 6 bits */
+    color = grey_5bit << 11 | grey_5bit << (5 + 1) | grey_5bit;
+    break;
+  }
+  return color;
+}
+
+static void fill_buffer_rgb565(enum corner corner, uint8_t grey, uint8_t *buf,
+                               size_t buf_size) {
+  uint16_t color = get_rgb565_color(corner, grey);
+
+  for(size_t idx = 0; idx < buf_size; idx += 2) {
+    *(buf + idx + 0) = (color >> 8) & 0xFFu;
+    *(buf + idx + 1) = (color >> 0) & 0xFFu;
+  }
+}
+
+void view_lvgl() {
+  size_t x;
+  size_t y;
+  size_t rect_w;
+  size_t rect_h;
+  size_t h_step;
+  size_t scale;
+  size_t grey_count;
+  uint8_t *buf;
+  int32_t grey_scale_sleep;
+  const struct device *display_dev;
+  struct display_capabilities capabilities;
+  struct display_buffer_descriptor buf_desc;
+  size_t buf_size = 0;
+  fill_buffer fill_buffer_fnc = NULL;
+
+  display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+  if (!device_is_ready(display_dev)) {
+    printk("Device %s not found. Aborting sample.",
+      display_dev->name);
+    return;
+  }
+
+  display_get_capabilities(display_dev, &capabilities);
+
+  if (capabilities.screen_info & SCREEN_INFO_MONO_VTILED) {
+    rect_w = 16;
+    rect_h = 8;
+  } else {
+    rect_w = 2;
+    rect_h = 1;
+  }
+
+  h_step = rect_h;
+  scale = (capabilities.x_resolution / 8) / rect_h;
+
+  rect_w *= scale;
+  rect_h *= scale;
+
+  if (capabilities.screen_info & SCREEN_INFO_EPD) {
+    grey_scale_sleep = 10000;
+  } else {
+    grey_scale_sleep = 100;
+  }
+
+  buf_size = rect_w * rect_h;
+
+  if (buf_size < (capabilities.x_resolution * h_step)) {
+    buf_size = capabilities.x_resolution * h_step;
+  }
+
+  switch (capabilities.current_pixel_format) {
+    case PIXEL_FORMAT_ARGB_8888:
+      fill_buffer_fnc = NULL;
+      buf_size *= 4;
+      break;
+    case PIXEL_FORMAT_RGB_888:
+      fill_buffer_fnc = NULL;
+      buf_size *= 3;
+      break;
+    case PIXEL_FORMAT_RGB_565:
+      fill_buffer_fnc = fill_buffer_rgb565;
+      buf_size *= 2;
+      break;
+    case PIXEL_FORMAT_BGR_565:
+      fill_buffer_fnc = NULL;
+      buf_size *= 2;
+      break;
+    case PIXEL_FORMAT_MONO01:
+    case PIXEL_FORMAT_MONO10:
+      fill_buffer_fnc = NULL;
+      buf_size /= 8;
+      break;
+    default:
+      printk("Unsupported pixel format. Aborting sample.");
+      return;
+  }
+
+  buf = k_malloc(buf_size);
+
+  if (buf == NULL) {
+    printk("Could not allocate memory. Aborting sample.");
+    return;
+  }
+
+  (void)memset(buf, 0xFFu, buf_size);
+
+  buf_desc.buf_size = buf_size;
+  buf_desc.pitch = capabilities.x_resolution;
+  buf_desc.width = capabilities.x_resolution;
+  buf_desc.height = h_step;
+
+  for (int idx = 0; idx < capabilities.y_resolution; idx += h_step) {
+    display_write(display_dev, 0, idx, &buf_desc, buf);
+  }
+
+  buf_desc.pitch = rect_w;
+  buf_desc.width = rect_w;
+  buf_desc.height = rect_h;
+
+  printk("Display sample for %s", display_dev->name);
+  printk("buf_size: %i\n", buf_size);
+  printk("x_res: %i\n", capabilities.x_resolution);
+  printk("y_res: %i\n", capabilities.y_resolution);
+  printk("rect_h: %i\n", rect_h);
+  printk("pitch / rect_w: %i\n", rect_w);
+
+  fill_buffer_fnc(TOP_LEFT, 0, buf, buf_size);
+  x = 0;
+  y = 0;
+  printk("sets y to draw: %i\n", y);
+  display_write(display_dev, x, y, &buf_desc, buf);
+
+  fill_buffer_fnc(TOP_RIGHT, 0, buf, buf_size);
+  x = capabilities.x_resolution - rect_w;
+  y = 0;
+  printk("sets y to draw: %i\n", y);
+  display_write(display_dev, x, y, &buf_desc, buf);
+
+  fill_buffer_fnc(BOTTOM_RIGHT, 0, buf, buf_size);
+  x = capabilities.x_resolution - rect_w;
+  y = capabilities.y_resolution - rect_h;
+  printk("sets y to draw: %i\n", y);
+  display_write(display_dev, x, y, &buf_desc, buf);
+
+  display_blanking_off(display_dev);
+
+  grey_count = 0;
+  x = 0;
+  y = capabilities.y_resolution - rect_h;
+
+  while (1) {
+    fill_buffer_fnc(BOTTOM_LEFT, grey_count, buf, buf_size);
+    display_write(display_dev, x, y, &buf_desc, buf);
+    ++grey_count;
+    k_msleep(grey_scale_sleep);
+    if (grey_count >= 1024) {
+      break;
+    }
+  }
+
+  return;
+}
